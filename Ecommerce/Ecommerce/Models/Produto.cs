50544fdfa1b1cfb4b2d4141f5c585774e28caeb6
Aluno <Aluno@DOSO121764>
2018-08-07 09:42:22 -0300

    fim da primeira etapa da aula

--- Ecommerce/Ecommerce/Models/Produto.cs
+++ Ecommerce/Ecommerce/Models/Produto.cs
@@ -11,11 +11,26 @@ namespace Ecommerce.Models
     public class Produto
     {
         [Key]
+        
         public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage ="Campo obrigatorio !")]
+        [MaxLength(50, ErrorMessage ="O Campo deve ter no máximo de 50 caracteries")]
+        [Display(Name = "nome do produto")]
         public String Nome {get; set; }
+
+        [Display(Name = "descrição do produto")]
+        [DataType (DataType.MultilineText)]
         public String  Descricao { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio !")]
+        [Display(Name = "preço do produto")]
         public double Preco { get; set; }
+
+        [Display(Name = "categoria do produto")]
         public String  Categoria { get; set; }
+
+
         public String Imagem { get; set; }
          
 
